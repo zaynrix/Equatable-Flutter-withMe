aeda585e047e55f7f9e7511146a838f64e25f70f
mac <zayns1914@gmail.com>
2023-08-06 13:55:38 +0200

    pr

---
+++ android/app/src/main/kotlin/com/example/equatableflutterdev/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.equatableflutterdev
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/phone_model.dart
@@ -0,0 +1,24 @@
+// import 'package:equatable/equatable.dart';
+//
+// class Phone extends Equatable {
+//   final String phoneName;
+//   final String phoneImage;
+//
+//   const Phone({
+//     required this.phoneName,
+//     required this.phoneImage,
+//   });
+//
+//   @override
+//   List<Object?> get props => [phoneName, phoneImage];
+// }
+
+class Phone {
+  final String phoneName;
+  final String phoneImage;
+
+  const Phone({
+    required this.phoneName,
+    required this.phoneImage,
+  });
+}
